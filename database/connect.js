7354e99773717bfa85d4acbc28054a15bda79edc
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2024-01-21 17:22:09 -0300

    add

--- database/connect.js
+++ database/connect.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
 // Conectar ao MongoDB Local
-//mongoose.connect(`mongodb://localhost:27017/minedev`);
-mongoose.connect(`mongodb+srv://mineDevBlogUser:tl2R8rYQ3iul9S2y@cluster0.baicaa5.mongodb.net/?retryWrites=true&w=majority`);
-
-
-
-// Verificar se a conexão foi bem-sucedida
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro na conexão:'));
-db.once('open', () => {
-    console.log('Conectado ao MongoDB!');
-});
+//mongoose.connect(`mongodb://localhost:27017/minedev`)
+mongoose.connect(`mongodb+srv://mineDevBlogUser:tl2R8rYQ3iul9S2y@cluster0.baicaa5.mongodb.net/?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log('Conectado ao MongoDB!')
+})
+.catch((erro)=>{
+    console.log('ERRO ao se conectado ao MongoDB!');
+})
 
 exports.mongoose = mongoose
--- routes/post.js
+++ routes/post.js
@@ -10,6 +10,26 @@ router.get('/novo', async (req, res) => {
 });
   
 
+router.get('/edit/:title', async (req, res) => {
+  try {
+    const title = req.params.title;
+    const urlTitle = title.replace(/-/g, ' ');
+
+    const data = await postSchema.findOne({'metadata.title': urlTitle});
+
+    if (!data) {
+      res.status(404).json({ msg: 'Post não encontrado' });
+      return;
+    }
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ msg: 'Erro na consulta do post: ' + err.message });
+  }
+});
+  
+  
+
 //PUBLICAR POST
 router.post('/new', (req, res) => {
   const newPost = new postSchema(req.body);
